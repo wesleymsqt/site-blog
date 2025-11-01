6724c79ce860aba486fa125ae6c708e8d8dd28b5
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-11-01 10:58:02 -0300

    feat: Creating the component layout and importing it into the app.tsx file.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/header";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -13,8 +12,8 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return (
-    <>
-      <Header />
-    </>
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    ></div>
   );
 }
