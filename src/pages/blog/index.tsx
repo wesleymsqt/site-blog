7030ae7f53df6c846d12147411d90c170a0b67fe
Wesley Mesquita <wesleymsqt@alu.ufc.br>
2025-11-09 15:38:08 -0300

    feat: add blog page with header and title for business strategies

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,18 @@
+export default function BlogPage() {
+  return (
+    <div className="flex flex-col py-24 flex-grow h-full">
+      <header className="">
+        <div className="container space-y-6 flex flex-col items-start justify-between md:flex-row md:items-end lg:items-end">
+          <div className="flex flex-col gap-4 md:px-0">
+            <span className="text-body-tag text-cyan-100 w-fit rounded-md text-center md:text-left py-2 px-4 bg-cyan-300">
+              BLOG
+            </span>
+            <h1 className="text-balance text-start md:text-left text-heading-lg md:text-heading-xl max-w-2xl text-gray-100">
+              Dicas e estrategias para impulsionar seu negocio
+            </h1>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
